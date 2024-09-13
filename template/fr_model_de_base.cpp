527de2a7e0df642b4d7147216b2d0be8e461938c
Patrick Saint-Louis <patricksaintlouis1@gmail.com>
2024-09-13 14:26:04 -0400

    Create fr_model_de_base.cpp

---
+++ template/fr_model_de_base.cpp
@@ -0,0 +1,21 @@
+/*
+LIRE ,
+CALCULER et ÉCRIRE .
+*/
+#include <iostream>
+using namespace std ;
+int main()
+{
+    //Declarer les variables et constantes et initialiser
+    
+
+    //Inviter, lire, et enregistrer les entrées 
+    cout << "  " ;
+    cin >>  ;
+
+    //Calculer
+   
+
+    //Afficher les sorties
+    cout << ;
+}
