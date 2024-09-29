18a3b82cd9efe39e49d51a3e09bb757b42f7e8de
Patrick Saint-Louis <patricksaintlouis1@gmail.com>
2024-09-28 22:48:16 -0400

    Update evaluer_questions_athletes_reponse_appropriee_vers1.cpp

--- w4/FR/w4_lab7_athlete_champion_ou_non/evaluer_questions_athletes_reponse_appropriee_vers1.cpp
+++ w4/FR/w4_lab7_athlete_champion_ou_non/evaluer_questions_athletes_reponse_appropriee_vers1.cpp
@@ -1,16 +1,17 @@
 /*
-LIRE le prénom et le nom,
-CALCULER et ÉCRIRE le nom complet qui est une combinaison des deux.
+LIRE 2 REPONSES,
+CALCULER ET ECRIRE UN MESSAGE APPROPRIE
+EN UTILISANT 1 SEULE INSTRUCTION IF.
 */
 #include <iostream>
 using namespace std ;
 int main()
 {
-//Déclarer les variables et les constantes et initialiser
+//DÃ©clarer les variables et les constantes et initialiser
     char reponse_question1, reponse_question2;
     const string MESSAGE_COMMUN = "MERCI D'AVOIR PARTICIPE A NOTRE TOURNOI DE JEUX SPORTIFS !";
     string message_champion = "";
-//Inviter, lire et enregistrer les entrées
+//Inviter, lire et enregistrer les entrÃ©es
     cout << "AVEZ-VOUS RENCONTRE D'AUTRES ATHLETES? (O/N)" ;
     cout << endl ; //Saut de ligne
     cin >> reponse_question1;
